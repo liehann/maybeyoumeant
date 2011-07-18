ece55569497ed84c4df4a0dfd9a99f911ff493a0
Jan Lelis <mail@janlelis.de>
2011-07-18 11:08:38 +0200

    replace class variables with class instance variables

--- lib/maybeyoumeant.rb
+++ lib/maybeyoumeant.rb
@@ -11,30 +11,30 @@ require 'maybeyoumeant/std_err_logger'
 require 'maybeyoumeant/config'
 
 module MaybeYouMeant
-  @@logger = StdErrLogger.new
+  @logger = StdErrLogger.new
   def self.logger=(logger)
-    @@logger = logger
+    @logger = logger
   end
 
   def self.log(msg = nil, &block)
     # msg ||= yield if block_given?
-    @@logger.log(msg, &block)
+    @logger.log(msg, &block)
   end
 
 
   def self.tweak_history(method, nearby)
     begin
-      @@irb ||= Kernel.const_get('IRB') && Kernel.const_get('Readline') && Readline.const_defined?('HISTORY')
+      @irb ||= Kernel.const_get('IRB') && Kernel.const_get('Readline') && Readline.const_defined?('HISTORY')
     rescue NameError
-      @@irb = false
+      @irb = false
     end
-    return unless @@irb
+    return unless @irb
 
     line = Readline::HISTORY[-1]
     Readline::HISTORY.pop if Config::remove_from_history
 
     if Config.add_to_history
-      # Try to match .#{method}\W before replacing all occurences of method.
+      # Try to match .#{method}\W before replacing all occurrences of method.
       line.gsub!(/(\W|^)#{method}((?=\W)|$)/, "\\1#{nearby}")
       log("Maybe you meant: #{line}".foreground(:black).bright)
       Readline::HISTORY.push line
--- lib/maybeyoumeant/config.rb
+++ lib/maybeyoumeant/config.rb
@@ -15,19 +15,19 @@ module MaybeYouMeant::Config
     MaybeYouMeant::StdErrLogger === MaybeYouMeant.logger
   end
 
-  @@call_nearby = true
+  @call_nearby = true
 
   # When true if a nearby method is found it is automatically called.
   # Defaults to true.
   def self.call_nearby=(enabled)
-    @@call_nearby = enabled
+    @call_nearby = enabled
   end
 
   def self.call_nearby
-    @@call_nearby
+    @call_nearby
   end
 
-  @@add_to_history = true
+  @add_to_history = true
 
   # When true and a nearby method is called the history is manipulated to
   # have the nearby method name. Defaults to true.
@@ -36,24 +36,24 @@ module MaybeYouMeant::Config
   # Say object foo has a method 'foob' you will get the following in your history:
   # foo.foo -> foob.foob
   def self.add_to_history=(enabled)
-    @@add_to_history = true
+    @add_to_history = true
   end
 
   def self.add_to_history
-    @@add_to_history
+    @add_to_history
   end
 
-  @@remove_from_history = false
+  @remove_from_history = false
 
   # When true and a nearby method is called the history is manipulated to
   # have the line with the incorrect method name removed.
   # Defaults to false.
   def self.remove_from_history(enabled)
-    @@remove_from_history = enabled
+    @remove_from_history = enabled
   end
 
   def self.remove_from_history
-    @@remove_from_history
+    @remove_from_history
   end
 
 end
